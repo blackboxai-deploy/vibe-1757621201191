79ff814568e6d15d42d16915ed93a4b3f2615897
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-12 01:36:49 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import PaginaUnidades from "@/components/PaginaUnidades";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <PaginaUnidades />
+    </main>
+  );
+}
